84de30f8aa794341af5fc49cfc526e3f976a597b
diego40g <diegopaznaula@hotmail.com>
2022-05-13 01:51:45 -0500

    destructuring of JSON

--- Nivelacion_JS/03-JSON/js/json.js
+++ Nivelacion_JS/03-JSON/js/json.js
@@ -13,5 +13,20 @@ const User = {
     age:24,
     email:"diegopaznaula@gmail.com"
 }
-
 document.write(User.name);
+
+/*sin Destructuring*/
+document.write("<p>"+User.name+"</p>");
+document.write("<p>"+User.age+"</p>");
+document.write("<p>"+User.email+"</p>");
+
+/*con Destructuring*/
+const {nombre,edad,correo,ciudad}={
+    nombre: "Ignacio",
+    edad: 20,
+    correo:"dipaz@espe.edu.ec"
+}
+document.write("<p>"+nombre+"</p>");
+document.write("<p>"+edad+"</p>");
+document.write("<p>"+correo+"</p>");
+document.write("<p>"+ciudad+"</p>");
