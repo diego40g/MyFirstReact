6e49b4fb827145a967e11350fae78ec910313824
diego40g <diegopaznaula@hotmail.com>
2022-05-12 22:17:24 -0500

    fatarrow functions with return o not

---
+++ Nivelacion_JS/01-functions/js/functions.js
@@ -0,0 +1,10 @@
+const resta = (a,b) => a - b;
+const sumar = (a, b) => {
+    return a + b;
+}
+
+const resultado1 = sumar(5,3);
+document.write("<p>La respuesta de la suma es = "+resultado1+"</p>");
+
+const resultado2 = resta(5,3);
+document.write("<p>La respuesta de la resta es = "+resultado2+"</p>");
