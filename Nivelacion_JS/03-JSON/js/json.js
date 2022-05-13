f613fb4501cf4967f30ff4276cfb4ef1b58663c3
diego40g <diegopaznaula@hotmail.com>
2022-05-12 22:54:49 -0500

    json examples

---
+++ Nivelacion_JS/03-JSON/js/json.js
@@ -0,0 +1,17 @@
+var Ball = {
+    color: "red",
+    size: 7,
+    jump: false,
+    setJump: function(){
+        this.jump=true
+    }
+}
+console.log(Ball);
+
+const User = {
+    name:"Diego",
+    age:24,
+    email:"diegopaznaula@gmail.com"
+}
+
+document.write(User.name);
