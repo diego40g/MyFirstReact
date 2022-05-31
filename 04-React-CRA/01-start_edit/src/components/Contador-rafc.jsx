8d1ccc4cea9c619c9c82310950962bee80bb939c
diego40g <diegopaznaula@hotmail.com>
2022-05-30 23:47:58 -0500

    import components

---
+++ 04-React-CRA/01-start_edit/src/components/Contador-rafc.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Contador = () => {
+  return (
+    <div>
+        <h1>Contador</h1>
+    </div>
+  )
+}
---
+++ 04-React-CRA/01-start_edit/src/components/Contador-rafce.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contador = () => {
+  return (
+    <div>
+        <h1>Contador: 0</h1>
+    </div>
+  );
+};
+
+export default Contador
