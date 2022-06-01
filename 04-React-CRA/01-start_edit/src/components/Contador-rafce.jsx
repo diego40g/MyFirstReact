8a1557c52628e484704826bf9e30b8c8c88ba8f3
diego40g <diegopaznaula@hotmail.com>
2022-05-31 22:10:47 -0500

    add button and funtionality add and rest

--- 04-React-CRA/01-start_edit/src/components/Contador-rafce.jsx
+++ 04-React-CRA/01-start_edit/src/components/Contador-rafce.jsx
@@ -3,11 +3,26 @@ import React, { useState } from 'react'
 const Contador = () => {
   //console.log(React);
   const [rContador, setRContador] = React.useState(101)
-  const [contador, setContador] = useState(101);
+  const [contador, setContador] = useState(0);
+
+  const aumentar=()=>{
+    setContador((actual)=>{//valor es el estado actual
+      console.log(actual)
+      return actual+1
+    })
+  }
+  const disminuir=()=>{
+    setContador(contador-1)
+  }
+
   return (
     <div>
         <h1>Contador-React: {rContador}</h1>
         <h1>Contador: {contador}</h1>
+        <hr/>
+
+        <button onClick={aumentar}>Aumentar</button>
+        <button onClick={disminuir}>Disminuir</button>
     </div>
   );
 };
