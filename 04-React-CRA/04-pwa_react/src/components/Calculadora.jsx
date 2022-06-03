85bbf62c9e8c228f8c7b6d62441268b4743ae864
diego40g <diegopaznaula@hotmail.com>
2022-06-02 22:57:53 -0500

    struct of number and result

--- 04-React-CRA/04-pwa_react/src/components/Calculadora.jsx
+++ 04-React-CRA/04-pwa_react/src/components/Calculadora.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Calculadora = () => {
+  const [suma, setSuma] = useState(0);
   return (
     <div>
         <label>
             Número 1: 
             <input type="text" />
         </label>
+        <label>
+            Número 2: 
+            <input type="text" />
+        </label>
+        <br/>
+        <span>Suma: {suma}</span>
     </div>
   )
 }
