cb39275bd29a3c6cb8c1c56029d873ebfe3d7496
diego40g <diegopaznaula@hotmail.com>
2022-06-02 22:52:37 -0500

    new componet with label and input

---
+++ 04-React-CRA/04-pwa_react/src/components/Calculadora.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Calculadora = () => {
+  return (
+    <div>
+        <label>
+            Número 1: 
+            <input type="text" />
+        </label>
+    </div>
+  )
+}
+
+export default Calculadora
