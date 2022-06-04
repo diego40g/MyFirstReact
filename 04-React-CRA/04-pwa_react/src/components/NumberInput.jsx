f7a61661396702b5b5e9eb753c6f3e57edceb2c4
diego40g <diegopaznaula@hotmail.com>
2022-06-04 13:13:03 -0500

    type float

--- 04-React-CRA/04-pwa_react/src/components/NumberInput.jsx
+++ 04-React-CRA/04-pwa_react/src/components/NumberInput.jsx
@@ -2,18 +2,24 @@ import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const NumberInput = ({name}) => {
-  const [numeros, setNumeros] = useState({
+  /*const [numeros, setNumeros] = useState({
     numero1:0,
     numero2:0
-  });
+  });*/
+  const [numeros, setNumeros] = useState(0)
   const handleChange=(e)=>{
       //console.log(e)
-      setNumeros(e.target.value)
+      //setNumeros(e.target.value)
+      /*setNumeros({
+        numero1: e.target.value,
+        numero2: e.target.value
+      })*/
+      setNumeros(parseFloat(e.target.value));
   }
   return (
     <>
         <label>
-            {name}: <input onChange={handleChange} type="text" />
+            {name}: <input value={numeros} onChange={handleChange} type="number" />
         </label>
     </>
   )
