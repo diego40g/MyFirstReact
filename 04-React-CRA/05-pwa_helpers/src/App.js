fdc1dc813af43bbbfd84d11366b77cbd2581c4a2
diego40g <diegopaznaula@hotmail.com>
2022-06-05 20:21:29 -0500

    using helpers to use functions

---
+++ 04-React-CRA/05-pwa_helpers/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Calculadora from './components/Calculadora'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Calculadora React PWA</h1>
+      <hr/>
+      <Calculadora/>
+    </div>
+  )
+}
+
+export default App
---
+++ 04-React-CRA/05-pwa_helpers/src/components/NumberInput.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import Result from './Result'
+import { operations } from '../helpers/operations'
+
+const NumberInput = () => {
+  const [numeros, setNumeros] = useState({
+    numero1:0,
+    numero2:0
+  });
+  
+  const {handleChange,sumar,restar,multiplicar,dividir,numero1,numero2} = operations(numeros,setNumeros)
+
+  return (
+    <>
+        <label>
+            Número 1:{""} 
+            <input name="numero1" value={numero1} onChange={handleChange} type="number" />
+        </label>
+        <label>
+            Número 2: {""}
+            <input name="numero2" value={numero2} onChange={handleChange} type="number" />
+        </label>
+        <hr />
+        <Result operacion='Suma' calculo={sumar()}/>
+        <Result operacion='Resta' calculo={restar()}/>
+        <Result operacion='Multiplicación' calculo={multiplicar()}/>
+        <Result operacion='Divición' calculo={dividir()}/>
+    </>
+  )
+}
+
+NumberInput.propTypes = {
+    name:PropTypes.string
+}
+
+export default NumberInput
