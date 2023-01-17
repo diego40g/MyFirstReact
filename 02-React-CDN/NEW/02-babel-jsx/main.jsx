8ab52073bd6bcc2ebdfcf2fe46c646b83abd9b1c
diego40g <diegopaznaula@hotmail.com>
2023-01-16 22:51:14 -0500

    use html tag into jsx with babel

---
+++ 02-React-CDN/NEW/02-babel-jsx/main.jsx
@@ -0,0 +1,13 @@
+const container = document.querySelector("#root")
+const root = ReactDOM.createRoot(container)
+
+root.render(
+    <div>
+        <ol>
+            <li>Hola</li>    
+            <li>Elemento 2</li>    
+            <li>Aprendiendo React</li>    
+            <li>Usando Jsx</li>    
+        </ol>
+    </div>
+)
