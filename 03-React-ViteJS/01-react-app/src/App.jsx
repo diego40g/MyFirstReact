ccbc681b376155605fe46877b9d7961396f2c2df
diego40g <diegopaznaula@hotmail.com>
2023-01-18 15:47:20 -0500

    change struct html with two buttons increase and decrease

--- 03-React-ViteJS/01-react-app/src/App.jsx
+++ 03-React-ViteJS/01-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const nombre="Diego"
   return (
     <div className="App">
@@ -12,9 +12,14 @@ function App() {
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <h1>{nombre}</h1>
-        <pre>{new Date().getDate()}</pre>
+        <p>
+          <button> - </button>
+          <h1>10</h1>
+          <button> + </button>
+        </p>
       </header>
+      <h2>{nombre}</h2>
+      <pre>{new Date().getDate() +"/"+ new Date().getMonth() +"/"+ new Date().getFullYear()}</pre>
     </div>
   )
 }
