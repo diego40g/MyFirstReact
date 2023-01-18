1e8bc7cba9aa0921511e91456e80e3eb96330c76
diego40g <diegopaznaula@hotmail.com>
2023-01-18 16:40:08 -0500

    use container div

--- 03-React-ViteJS/01-react-app/src/App.jsx
+++ 03-React-ViteJS/01-react-app/src/App.jsx
@@ -22,11 +22,11 @@ const App = () => {
         <a href="https://reactjs.org" width="250" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <p>
+        <div>
           <button onClick={disminuir}> - </button>
           <h1>{contador}</h1>
           <button onClick={aumentar}> + </button>
-        </p>
+        </div>
       </header>
       <h2>{nombre}</h2>
       <pre>{new Date().getDate() +"/"+ new Date().getMonth() +"/"+ new Date().getFullYear()}</pre>
