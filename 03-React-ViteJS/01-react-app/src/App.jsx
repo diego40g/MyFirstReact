bcb2968b250e667f702edb43fa6012c0d2a72d88
diego40g <diegopaznaula@hotmail.com>
2023-01-18 16:37:23 -0500

    create a counter with useState

--- 03-React-ViteJS/01-react-app/src/App.jsx
+++ 03-React-ViteJS/01-react-app/src/App.jsx
@@ -1,21 +1,31 @@
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 const App = () => {
   const nombre="Diego"
+
+  const [contador, setContador] = useState(10)
+
+  const disminuir = () => {
+    setContador(contador-1)
+  }
+  const aumentar = () => {
+    setContador(contador+1)
+  }
   return (
     <div className="App">
       <header>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" width="250" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" width="250" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
         <p>
-          <button> - </button>
-          <h1>10</h1>
-          <button> + </button>
+          <button onClick={disminuir}> - </button>
+          <h1>{contador}</h1>
+          <button onClick={aumentar}> + </button>
         </p>
       </header>
       <h2>{nombre}</h2>
