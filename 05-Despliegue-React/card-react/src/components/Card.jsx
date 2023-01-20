16d7ee4733a96542a223d9f5126e91427fe608c5
diego40g <diegopaznaula@hotmail.com>
2023-01-20 18:28:41 -0500

    the final card struct and style

--- 05-Despliegue-React/card-react/src/components/Card.jsx
+++ 05-Despliegue-React/card-react/src/components/Card.jsx
@@ -5,8 +5,10 @@ import "./Card.css"
 const Card = () => {
   return (
     <div className='card'>
-        <img src="https://cdn.svgporn.com/logos/python.svg" alt="lang.svg" />
-        <h3>Lenguaje</h3>
+        <img src="https://cdn.svgporn.com/logos/python.svg" alt="python image" />
+        <h3>
+          Python
+        </h3>
     </div>
   )
 }
