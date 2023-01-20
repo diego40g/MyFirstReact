0698ce82737e6711f4ee4ccad7e324a5a4e0aa33
diego40g <diegopaznaula@hotmail.com>
2023-01-20 18:32:24 -0500

    add cards who contain different card

--- 05-Despliegue-React/card-react/src/App.jsx
+++ 05-Despliegue-React/card-react/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import Contador from "./components/Contador"
-import Card from './components/Card'
+import Cards from './components/Cards'
 
 function App() {
   return (
     <>
     <Contador inicial={5} factor={3} />
-    <Card/>
+    <Cards/>
     </>
   )
 }
