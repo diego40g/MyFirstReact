2bb59e5702b6b18213cf9b4414c1a916d4ea72c8
diego40g <diegopaznaula@hotmail.com>
2023-01-20 18:01:42 -0500

    adding style of cards

--- 05-Despliegue-React/card-react/src/components/Card.jsx
+++ 05-Despliegue-React/card-react/src/components/Card.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import "./Card.css"
+
 const Card = () => {
   return (
-    <>
-        <img src="#" alt="lang.svg" />
-        <h3>Lenguahe</h3>
-    </>
+    <div className='card'>
+        <img src="https://cdn.svgporn.com/logos/python.svg" alt="lang.svg" />
+        <h3>Lenguaje</h3>
+    </div>
   )
 }
 
