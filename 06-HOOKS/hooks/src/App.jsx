0a38a6a7d9377b4a4186d2a4edf2bbe80df01116
diego40g <diegopaznaula@hotmail.com>
2023-01-24 19:18:45 -0500

    default config for calculator app

---
+++ 06-HOOKS/hooks/src/App.jsx
@@ -0,0 +1,14 @@
+import Calculadora from "./components/Calculadora";
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="container text-center">
+      <h1>Calculadora React PWA</h1>
+      <hr />
+      <Calculadora/>
+    </div>
+  )
+}
+
+export default App
---
+++ 06-HOOKS/hooks/src/components/Calculadora.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NumberInput from './NumberInput';
+
+const Calculadora = () => {  
+  return (
+    <div>
+        <NumberInput name="Número 1"/>
+    </div>
+  )
+}
+
+export default Calculadora
