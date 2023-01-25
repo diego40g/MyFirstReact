6b0b2e1f1f0111ba3c978b39474c04ad29a4de65
diego40g <diegopaznaula@hotmail.com>
2023-01-25 17:35:49 -0500

    how to use useState

--- 06-HOOKS/hooks/src/components/State-use.jsx
+++ 06-HOOKS/hooks/src/components/State-use.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { useState } from 'react'
 
 const StateUse = () => {
-  const state = useState()
+  const [state, setState] = useState(0)
+
+  setInterval(() => {
+    console.log(state);
+    setState(state +1)
+  }, 3000)
   return (
     <div className='container text-center'>
         <h1>UseState</h1>
         <hr />
+        <p>Cuenta: {state} </p>
     </div>
   )
 }
