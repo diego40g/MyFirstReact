0e4abed6159f602f7dc908c2eead93e07240d359
diego40g <diegopaznaula@hotmail.com>
2023-01-25 23:07:14 -0500

    call only de new component form with prevent default

--- 06-HOOKS/hooks/src/App.jsx
+++ 06-HOOKS/hooks/src/App.jsx
@@ -2,13 +2,15 @@ import Calculadora from "./components/Calculadora";
 import StateUse from "./components/State-use"
 import EffectUse from "./components/Effect-use"
 import './App.css'
+import FormPreventDefault from "./components/Form-PreventDefault";
 
 const App = () => {
   return (
-    <>
-    <StateUse/>
-    <EffectUse/>
-    </>
+    <div className="container text-center">
+    {/* <StateUse/>
+    <EffectUse/> */}
+    <FormPreventDefault/>
+    </div>
     // <div className="container text-center">
     //   <h1>Calculadora React PWA</h1>
     //   <hr />
