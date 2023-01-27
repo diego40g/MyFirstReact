26d0d0a09e67f64de95b5e92bb8cdaa931fc671e
diego40g <diegopaznaula@hotmail.com>
2023-01-26 23:11:54 -0500

    add component of async effect in content

--- 06-HOOKS/hooks/src/App.jsx
+++ 06-HOOKS/hooks/src/App.jsx
@@ -3,13 +3,14 @@ import StateUse from "./components/State-use"
 import EffectUse from "./components/Effect-use"
 import './App.css'
 import FormPreventDefault from "./components/Form-PreventDefault";
+import EffectAsync from "./components/Effect-async";
 
 const App = () => {
   return (
     <div className="container text-center">
     {/* <StateUse/>
     <EffectUse/> */}
-    <FormPreventDefault/>
+    <EffectAsync/>
     </div>
     // <div className="container text-center">
     //   <h1>Calculadora React PWA</h1>
