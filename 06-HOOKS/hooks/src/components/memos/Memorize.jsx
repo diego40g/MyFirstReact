ae78d24be784db42d81500a774a493e672ccfb15
diego40g <diegopaznaula@hotmail.com>
2023-01-30 23:40:29 -0500

    add useMemo into memorize component

--- 06-HOOKS/hooks/src/components/memos/Memorize.jsx
+++ 06-HOOKS/hooks/src/components/memos/Memorize.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react'
+import React, { useMemo, useState} from 'react'
 import Dato from './Data'
 
 const Memorize = () => {
@@ -7,6 +7,14 @@ const Memorize = () => {
   const handleClick = () => {
     setCounter(counter + 1)
   }
+  const pesado = (iter) => {
+    for (let i=0; i<iter;i++){
+      console.log("procesando");
+    }
+    return "fin del proceso"
+  }
+  //const pesadoMemo = useMemo(() => pesado(counter), [counter])
+  useMemo(() => pesado(counter), [counter])
   return (
     <>
         <h1>Memorize: <Dato value={counter}/></h1>
