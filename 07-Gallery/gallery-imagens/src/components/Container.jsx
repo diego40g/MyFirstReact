b4db7ae804f2f93aeb949f06160aa8bbbae4ae7a
diego40g <diegopaznaula@hotmail.com>
2023-02-02 23:33:25 -0500

    create component container who has a card with image

---
+++ 07-Gallery/gallery-imagens/src/components/Container.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <div className='container border'>
+      <div className="card" style={{
+        width: "15rem"
+      }}>
+        <img src="https://images.unsplash.com/photo-1674558916626-9c6531f67cf5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYwMjZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzUzNzQ2Mjc&ixlib=rb-4.0.3&q=80&w=1080" className="card-img-top" alt="imagen punto png"/>
+      </div>  
+    </div>
+  )
+}
+
+export default Container
