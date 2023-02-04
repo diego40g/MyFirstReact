3f3f18d4d72e0646c97f0c93311914c68de75aef
diego40g <diegopaznaula@hotmail.com>
2023-02-03 23:00:10 -0500

    add component cards and delete the other container

--- 07-Gallery/gallery-imagens/src/components/Container.jsx
+++ 07-Gallery/gallery-imagens/src/components/Container.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
+import Cards from './Cards'
 
 const Container = () => {
   return (
     <div className='container border'>
-      <div className="card" style={{
-        width: "15rem"
-      }}>
-        <img src="https://images.unsplash.com/photo-1674558916626-9c6531f67cf5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MDYwMjZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzUzNzQ2Mjc&ixlib=rb-4.0.3&q=80&w=1080" className="card-img-top" alt="imagen punto png"/>
-      </div>  
+        <Cards/>  
     </div>
   )
 }
