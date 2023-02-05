dced2d519dcca5f2f73d8e23665ee38165542200
diego40g <diegopaznaula@hotmail.com>
2023-02-04 22:17:09 -0500

    add search images into array

--- 07-Gallery/gallery-imagens/src/components/Cards.jsx
+++ 07-Gallery/gallery-imagens/src/components/Cards.jsx
@@ -3,17 +3,23 @@ import Card from './Card'
 
 const Cards = () => {
   const [images, setImages] = useState([]);
-  const peticion = async () => {
-    //const res = await fetch(`https://api.unsplash.com/photos/?client_id=${import.meta.env.VITE_KEY_UNPLASH}`)
-    const data = await res.json()
+  const [input, setInput] = useState("")
 
-    //console.log(data)
+  const peticion = async () => {
+    const key = `client_id=${import.meta.env.VITE_KEY_UNPLASH}`
+    let route = `https://api.unsplash.com/photos/?${key}`
+
+    if(input !== ""){
+      route = `https://api.unsplash.com/search/photos?page=1&query=${input}&${key}`
+    }
+
+    //const res = await fetch(route)
+    const data = await res.json()
     setImages(data)
   }
   useEffect(() => {
     peticion();
-  }, [])
-  const [input, setInput] = useState("second")
+  }, [input])
   const handleSubmit = (e) => {
     e.preventDefault()
     const text = e.target[0].value
