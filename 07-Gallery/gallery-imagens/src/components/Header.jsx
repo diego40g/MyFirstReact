b757fea76a4809c6d7bedc1b08497211429b2c93
diego40g <diegopaznaula@hotmail.com>
2023-02-05 23:13:17 -0500

    create component header who has the name of app

---
+++ 07-Gallery/gallery-imagens/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav class="navbar bg-primary" data-bs-theme="dark">
+        <div className="container-fluid">
+            <span className='navbar-brand w-100 mb-0 text-light h1'>
+                Gallery App
+            </span>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
