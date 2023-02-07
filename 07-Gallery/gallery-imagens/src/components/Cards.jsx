6c2d1fd459dc559cd8d5007587cefcc8c4b7dee5
diego40g <diegopaznaula@hotmail.com>
2023-02-06 19:58:29 -0500

    import useFetchimages for use in cards components

--- 07-Gallery/gallery-imagens/src/components/Cards.jsx
+++ 07-Gallery/gallery-imagens/src/components/Cards.jsx
@@ -1,45 +1,10 @@
-import React, { useCallback, useEffect, useState } from 'react'
 import Card from './Card'
 import Loading from './Loading'
 import FormImg from './FormImg'
+import { useFetchIamges } from '../hooks/useFetchImages'
 
 const Cards = () => {
-  const [images, setImages] = useState([]);
-  const [input, setInput] = useState("")
-  const [loading, setLoading] = useState(true)
-
-  const peticion = useCallback( async () => {
-    const key = `client_id=${import.meta.env.VITE_KEY_UNPLASH}`
-    let route = `https://api.unsplash.com/photos/?${key}`
-
-    if(input !== ""){
-      route = `https://api.unsplash.com/search/photos?page=1&query=${encodeURI(input)}&${key}`
-    }
-
-    setLoading(true)
-
-    //const res = await fetch(route)
-    const data = await res.json()
-
-    if (data.results) {
-      setImages(data.results)
-    }else{
-      setImages(data)
-    }
-
-    setLoading(false)
-  }, [input])
-
-  useEffect(() => {
-    peticion();
-  }, [peticion])
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const text = e.target[0].value
-
-    setInput(text)
-  }
+  const [images, loading, handleSubmit] = useFetchIamges()
   return (
     <div className='text-center'>
       <FormImg handleSubmit={handleSubmit}/>
