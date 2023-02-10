76adf8b5060c01218ce529955e8f61c02463c693
diego40g <diegopaznaula@hotmail.com>
2023-02-09 23:46:39 -0500

    add reducer to mange contacts

---
+++ 08-Reducer/contact-book-reducer/src/reducers/ContactosReducer.js
@@ -0,0 +1,8 @@
+export const ContactsReducer = (state, action) => {
+    switch (action.type) {
+        case "add":
+            return [...state, action.payload];
+        default:
+            return state;
+    }
+}
