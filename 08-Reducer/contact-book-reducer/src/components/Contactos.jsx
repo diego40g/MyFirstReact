bb328db6388bcac62ef5305d8861648416f9e072
diego40g <diegopaznaula@hotmail.com>
2023-02-09 23:47:18 -0500

    add Form and Table contact with pass of state of useReduce

---
+++ 08-Reducer/contact-book-reducer/src/components/Contactos.jsx
@@ -0,0 +1,35 @@
+import React, { useReducer } from 'react'
+import TableContacts from './TableContacts'
+import FormularioAdd from './FormularioAdd'
+import { ContactsReducer } from '../reducers/ContactosReducer'
+
+const contactos = [
+    {
+        id: "dp12",
+        nombre: "Diego",
+        numero: "213123"
+    },
+    {
+        id: "ip12",
+        nombre: "Ismael",
+        numero: "762132"
+    },
+    {
+        id: "an12",
+        nombre: "Ana",
+        numero: "659874"
+    },
+]
+
+const Contactos = () => {
+  const [state, dispatch] = useReducer(ContactsReducer, contactos)
+  
+  return (
+    <div className='container mt-3'>
+        <FormularioAdd/>
+        <TableContacts contactos={state}/>
+    </div>
+  )
+}
+
+export default Contactos
