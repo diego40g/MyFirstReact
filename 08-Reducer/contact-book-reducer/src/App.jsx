569064ef83082c8452b2ce2d3b05e3f49f4bddeb
diego40g <diegopaznaula@hotmail.com>
2023-02-09 23:47:33 -0500

    add component Header and Contact

--- 08-Reducer/contact-book-reducer/src/App.jsx
+++ 08-Reducer/contact-book-reducer/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './components/Header'
+import Contactos from './components/Contactos'
 
 const App = () => {
   return (
-    <div>Agenda React</div>
+    <>
+      <Header/>
+      <Contactos/>
+    </>
   )
 }
 
