635460d40af013c52a1f0cadadde16cccf819c6a
diego40g <diegopaznaula@hotmail.com>
2023-02-11 22:15:03 -0500

    use localstorage to save temporary  data

--- 08-Reducer/contact-book-reducer/src/components/Contactos.jsx
+++ 08-Reducer/contact-book-reducer/src/components/Contactos.jsx
@@ -1,28 +1,36 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react'
 import TableContacts from './TableContacts'
 import FormularioAdd from './FormularioAdd'
 import { ContactsReducer } from '../reducers/ContactosReducer'
 
-const contactos = [
-    {
-        id: "dp12",
-        nombre: "Diego",
-        numero: "213123"
-    },
-    {
-        id: "ip12",
-        nombre: "Ismael",
-        numero: "762132"
-    },
-    {
-        id: "an12",
-        nombre: "Ana",
-        numero: "659874"
-    },
-]
+// const contactos = [
+//     {
+//         id: "dp12",
+//         nombre: "Diego",
+//         numero: "213123"
+//     },
+//     {
+//         id: "ip12",
+//         nombre: "Ismael",
+//         numero: "762132"
+//     },
+//     {
+//         id: "an12",
+//         nombre: "Ana",
+//         numero: "659874"
+//     },
+// ]
+const init = () => {
+  const contactos = localStorage.getItem("contactos");
+  return contactos ? JSON.parse(contactos) : [];
+};
 
 const Contactos = () => {
-  const [state, dispatch] = useReducer(ContactsReducer, contactos)
+  //const [state, dispatch] = useReducer(ContactsReducer, contactos)
+  const [state, dispatch] = useReducer(ContactsReducer, [], init)
+  useEffect(() => {
+    localStorage.setItem("contactos", JSON.stringify(state))
+  }, [state])
   
   return (
     <div className='container mt-3'>
