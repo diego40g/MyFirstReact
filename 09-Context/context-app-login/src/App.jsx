409c17536b1cbb9e6872abb773f9b866cc5c6592
diego40g <diegopaznaula@hotmail.com>
2023-02-13 16:33:08 -0500

    change compnent header for approute

--- 09-Context/context-app-login/src/App.jsx
+++ 09-Context/context-app-login/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Header from './components/Header'
+import AppRouter from './routes/AppRouter'
 
 const App = () => {
   return (
     <>
-      <Header/>
+      <AppRouter/>
     </>
   )
 }
