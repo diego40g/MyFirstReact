b1e334c5b1c5243062672d52e14a982d83e445cf
diego40g <diegopaznaula@hotmail.com>
2023-02-14 17:22:33 -0500

    home page who yas a login and set data to context if null and existe create logout

--- 09-Context/context-app-login/src/pages/Home.jsx
+++ 09-Context/context-app-login/src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../contexts/UserContext'
+import { userData } from '../helpers/userData'
 
 const Home = () => {
+  const { user, setUser } = useContext(UserContext)
+  const handleLogin = () => {
+    setUser(userData)
+  }
+  const handleLogout = () => {
+    setUser(null)
+  }
   return (
-    <div>
-        <h1>Home</h1>
+    <div className='container text-center mt-5'>
+      <h1>Home Screen</h1>
+      {
+        !user ? 
+        <button className='btn btn-primary' onClick={handleLogin}>Login</button>
+        :
+        <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
+      }
     </div>
   )
 }
