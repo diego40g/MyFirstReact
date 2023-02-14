ef3c650309c122f37aae736ddc6073717a9cff89
diego40g <diegopaznaula@hotmail.com>
2023-02-14 17:23:19 -0500

    About pages who use userContext only get first_name, last_name and avatar of user Example

--- 09-Context/context-app-login/src/pages/About.jsx
+++ 09-Context/context-app-login/src/pages/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../contexts/UserContext'
 
 const About = () => {
+  const { user } = useContext(UserContext)
   return (
-    <div>
-        <h1>About</h1>
+    <div className='container text-center mt-5'>
+        <h1>About Screen</h1>
+        {
+          !user ?
+          <h2 className="text-danger">Please login</h2>
+          :
+          <div>
+            <h3>Complete Name: {user.data.first_name} {user.data.last_name}</h3>
+            <h3>Email {user.data.email}</h3>
+            <img src={user.data.avatar} alt={user.data.id+" "+user.data.first_name} width='150px' height='150px' />
+          </div>
+        }
     </div>
   )
 }
