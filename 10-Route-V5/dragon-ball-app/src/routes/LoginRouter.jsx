53efb9fad838c8609af60aa0ed312d8edb766e12
diego40g <diegopaznaula@hotmail.com>
2023-02-16 22:59:10 -0500

    It's using to control if the user is login or no to pass to use all app

---
+++ 10-Route-V5/dragon-ball-app/src/routes/LoginRouter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LoginScreen from '../pages/LoginScreen'
+import AppRouter from './AppRouter'
+
+const LoginRouter = () => {
+  return (
+    <Router>
+        <AppRouter/>
+        <Routes>
+            <Route exact path='/login' element={<LoginScreen/>}/>
+        </Routes>
+    </Router>
+  )
+}
+
+export default LoginRouter
