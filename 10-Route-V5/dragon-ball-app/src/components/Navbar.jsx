723a40032657788d4412f8f52f0b52a554ff9308
diego40g <diegopaznaula@hotmail.com>
2023-02-16 23:24:44 -0500

    navabr button to logout

--- 10-Route-V5/dragon-ball-app/src/components/Navbar.jsx
+++ 10-Route-V5/dragon-ball-app/src/components/Navbar.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate  } from 'react-router-dom'
 
 const Navbar = () => {
+  let navigate = useNavigate();
+  const handleLogout = () => {
+    navigate("/login");
+  }
   return (
     <nav className="navbar navbar-expand-lg bg-info">
         <div className="container-fluid">
-            <h2 className="navbar-brand" href="#">DB App</h2>
+            <a className="navbar-brand" href="/">
+                <h2>DB App</h2>
+            </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
+            <div className="navbar-nav me-auto mb-2 mb-lg-0">
                 <NavLink 
                     className={({isActive})=> isActive ? "nav-link active text-white" : "nav-link" }
                     aria-current="page" 
@@ -32,6 +38,9 @@ const Navbar = () => {
                 >
                     Search
                 </NavLink>
+            </div>
+            <div className="d-flex">
+                <button className='btn btn-danger' onClick={handleLogout}>Logout</button>
             </div>
             </div>
         </div>
