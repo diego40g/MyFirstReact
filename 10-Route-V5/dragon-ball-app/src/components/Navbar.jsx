d275b73f0fa7ffe62499d2042ea8c9d286c65af5
diego40g <diegopaznaula@hotmail.com>
2023-02-17 22:16:58 -0500

    change let to const for secure

--- 10-Route-V5/dragon-ball-app/src/components/Navbar.jsx
+++ 10-Route-V5/dragon-ball-app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink, useNavigate  } from 'react-router-dom'
 
 const Navbar = () => {
-  let navigate = useNavigate();
+  const navigate = useNavigate();
   const handleLogout = () => {
     navigate("/login");
   }
