eef00ec851b1f29b8505082fb24aebfd1f49bbe4
diego40g <diegopaznaula@hotmail.com>
2023-02-17 22:19:22 -0500

    mapping women characters and struct the information

--- 10-Route-V5/dragon-ball-app/src/pages/WomenScreen.jsx
+++ 10-Route-V5/dragon-ball-app/src/pages/WomenScreen.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Characters } from '../models/Characters'
+import Card from '../components/Card'
 
 const WomenScreen = () => {
+  const women = Characters.filter(character => character.type === "m")
   return (
-    <div>
+    <div className='container mt-3'>
         <h1>Women</h1>
+        <hr />
+        <div className="row">
+          {
+            women.map(
+              (woman) => (<Card key={woman.id} {...woman}/>)
+            )
+          }
+        </div>
     </div>
   )
 }
