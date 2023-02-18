0a5df42615453faf7899e722aea67288024a9523
diego40g <diegopaznaula@hotmail.com>
2023-02-17 22:28:09 -0500

    struct of search

--- 10-Route-V5/dragon-ball-app/src/pages/SearchScreen.jsx
+++ 10-Route-V5/dragon-ball-app/src/pages/SearchScreen.jsx
@@ -4,6 +4,25 @@ const SearchScreen = () => {
   return (
     <div>
         <h1>Search</h1>
+        <hr />
+        <div className="row">
+          <div className="col-6">
+            <h2>Search</h2>
+            <form>
+              <label className='form-label w-100'>
+                Character: {""}
+                <input 
+                  placeholder='Name character' 
+                  type="text" 
+                  className='form-control' 
+                  autoComplete='off' 
+                  value={""} 
+                />
+              </label>
+              <button type='submit' className='btn btn-info w-100'>Search</button>
+            </form>
+          </div>
+        </div>
     </div>
   )
 }
