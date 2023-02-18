69fb18eca954db34cf212acbd66dab2a70d0efc0
diego40g <diegopaznaula@hotmail.com>
2023-02-17 22:19:12 -0500

    mapping men characters and struct the information

--- 10-Route-V5/dragon-ball-app/src/pages/MenScreen.jsx
+++ 10-Route-V5/dragon-ball-app/src/pages/MenScreen.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Characters } from '../models/Characters'
+import Card from '../components/Card'
 
 const MenScreen = () => {
+  const men = Characters.filter(character => character.type === "h")
   return (
-    <div>
+    <div className='container mt-3'>
         <h1>Men</h1>
+        <hr />
+        <div className="row">
+          {
+            men.map(
+              (man) => (<Card key={man.id} {...man}/>)
+            )
+          }
+        </div>
     </div>
   )
 }
