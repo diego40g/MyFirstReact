6899a25de459adda0c3bc524d7a68a4d3d1504ad
diego40g <diegopaznaula@hotmail.com>
2023-02-19 23:09:34 -0500

    solve go back of use react route

--- 10-Route-V5/dragon-ball-app/src/pages/CharacterScreen.jsx
+++ 10-Route-V5/dragon-ball-app/src/pages/CharacterScreen.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { useParams } from 'react-router'
 import { Characters } from '../models/Characters';
 
-const CharacterScreen = (history) => {
+const CharacterScreen = ({history}) => {
   const {id} = useParams();
   const { type, name, description } = Characters.find(character => character.id === id)
   const path = `/public/assets/${type}-${id}.png`
   const handleBack = () => {
-    history(-1)
+    history.goBack()
   }
   return (
     <div className='container row mt-5'>
