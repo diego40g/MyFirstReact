d38a7d1d1fe76ca99b94ce4a3d9556ce22de1678
diego40g <diegopaznaula@hotmail.com>
2023-02-21 21:51:47 -0500

    change config to use hydrate and render for react-snap

--- 10-Route-V5/dragon-ball-app/src/main.jsx
+++ 10-Route-V5/dragon-ball-app/src/main.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM, { createRoot, hydrateRoot } from 'react-dom/client'
 import App from './App'
 import ReloadPrompt from './ReloadPrompt'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ReloadPrompt />
-    <App />
-  </React.StrictMode>,
-)
+const container = document.getElementById("root")
+
+if (container.hasChildNodes()){
+  const root = hydrateRoot(container,<><ReloadPrompt/><App/></>)
+} else {
+  const root = createRoot(container)
+  root.render(<><ReloadPrompt/><App/></>)
+}
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <ReloadPrompt />
+//     <App />
+//   </React.StrictMode>,
+// )
