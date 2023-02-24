9e447583be24aee8aef7db7e2c129e80a7ce7dce
diego40g <diegopaznaula@hotmail.com>
2023-02-23 23:26:03 -0500

    change Switch to Routes and use Route into the Routes to manage the auth login

--- 11-Route-V6/src/routes/LoginRouter.jsx
+++ 11-Route-V6/src/routes/LoginRouter.jsx
@@ -1,19 +1,25 @@
 import React, { useContext } from 'react'
-import { BrowserRouter as Router, Switch } from 'react-router-dom'
-import { AuthContext } from '../context/AuthContext'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginScreen from '../pages/LoginScreen'
 import AppRouter from './AppRouter'
 import PublicRouter from './PublicRouter'
 import PrivateRouter from './PrivateRouter'
 
 const LoginRouter = () => {
-  const {log} = useContext(AuthContext)
   return (
     <Router>
-      <Switch>
-        <PublicRouter path='/login' auth={log} component={LoginScreen}/>
-        <PrivateRouter path='/' auth={log} component={AppRouter}/>
-      </Switch>
+      <Routes>
+        <Route path="/login" element={
+          <PublicRouter>
+            <LoginScreen/>
+          </PublicRouter>
+        } />
+        <Route path="*" element={
+          <PrivateRouter>
+            <AppRouter/>
+          </PrivateRouter>
+        } />
+      </Routes>
     </Router>
   )
 }
