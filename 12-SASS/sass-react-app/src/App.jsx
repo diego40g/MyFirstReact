ae82151640ba2118f7b519ee33065d14672062ef
diego40g <diegopaznaula@hotmail.com>
2023-02-25 22:25:40 -0500

    add social networks and some button elements

--- 12-SASS/sass-react-app/src/App.jsx
+++ 12-SASS/sass-react-app/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Button from './components/Button'
 
 const App = () => {
   return (
     <div>
       <h1 className='title'>Hola SASS</h1>
+      <hr />
+      <ul>
+        <li>
+          <a className='link' href="#">Google</a>
+        </li>
+        <li>
+          <a className='link' href="#">Facebook</a>
+        </li>
+        <li>
+          <a className='link' href="#">Youtube</a>
+        </li>
+      </ul>
+      <hr />
+      <h2>Counter: 0</h2>
+      <Button type="btn" text="+1"/>
+      <Button type="btn btn-primary" text="-1"/>
     </div>
   )
 }
