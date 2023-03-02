99929ff2c73cf80c646c53f9a72c634755a7376f
diego40g <diegopaznaula@hotmail.com>
2023-03-02 11:48:20 -0500

    import functions of config-firebase and create login with Google

--- 13-Firebase-Redux-Materialize/salary-calculation/src/actions/auth.js
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/actions/auth.js
@@ -1,10 +1,29 @@
 import { types } from '../types/types'
-export const googleLogin = (id, username) => {
+import { signInWithPopup, googleAuhtProvider, auth, provider } from '../firebase/config-firebase'
+
+export const googleLogin = () => {
+    return (dispatch) => {
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = googleAuhtProvider.credentialFromResult(result)
+            const token  = credential.accessToken
+            const user = result.user;
+            dispatch(login(user.uid, user.displayName))
+        }).catch((error) => {
+            const errorCode = error.code
+            const errorMessage = error.message
+            const email = error.customData.email
+            const credential = googleAuhtProvider.credentialFromError(error)
+        })
+    }
+}
+
+export const login = (uid, displayname) => {
     return {
         type: types.login,
         payload: {
-            id,
-            username,
+            uid,
+            displayname,
         }
     }
 }
