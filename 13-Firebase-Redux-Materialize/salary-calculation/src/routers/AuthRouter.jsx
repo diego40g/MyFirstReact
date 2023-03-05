d664658b2c5d86727d5504948a25d23facb5d22e
diego40g <diegopaznaula@hotmail.com>
2023-03-04 23:59:22 -0500

    save user of last sing up

--- 13-Firebase-Redux-Materialize/salary-calculation/src/routers/AuthRouter.jsx
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/routers/AuthRouter.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+import {auth} from '../firebase/config-firebase'
+
 import LoginScreen from '../pages/LoginScreen'
 import RegisterScreen from '../pages/RegisterScreen'
+import { login } from '../actions/auth'
 
 const AuthRouter = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    auth.onAuthStateChanged(
+    (user) => {
+      dispatch(login(user.uid, user.displayName))
+    })
+  }, [])
+  
   return (
     <>
         <Router>
