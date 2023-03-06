bead29b33a304dccca655e2325bd2cf791ae7b60
diego40g <diegopaznaula@hotmail.com>
2023-03-05 22:25:48 -0500

    create app screen who was the navbar

---
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/pages/AppScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Navbar from '../components/Navbar'
+
+const AppScreen = () => {
+  return (
+    <>
+        <Navbar/>  
+    </>
+  )
+}
+
+export default AppScreen
