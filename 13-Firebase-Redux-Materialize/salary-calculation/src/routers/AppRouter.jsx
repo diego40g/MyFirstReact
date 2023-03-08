a59c3a23bcc940cd1a9a9203166de15ccc098327
diego40g <diegopaznaula@hotmail.com>
2023-03-07 23:05:30 -0500

    Route to validate the private routes to access it was singin

--- 13-Firebase-Redux-Materialize/salary-calculation/src/routers/AppRouter.jsx
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/routers/AppRouter.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import { Route, Routes } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
 import AppScreen from '../pages/AppScreen'
 
+import {auth} from '../firebase/config-firebase'
+import { login } from '../actions/auth'
+
+import AuthRouter from './AuthRouter'
+import PublicRouter from './PublicRouter'
+import PrivateRouter from './PrivateRouter'
+
 const AppRouter = () => {
+  const dispatch = useDispatch()
+  const [log, setLog] = useState(false)
+  useEffect(() => {
+    auth.onAuthStateChanged(
+    (user) => {
+      if(user){
+        dispatch(login(user.uid, user.displayName))
+        setLog(true)
+      } else {
+        setLog(false)
+      }
+    })
+  }, [dispatch])
+
   return (
-    <>
+    <Router>
+      <AuthRouter />
       <Routes>
-        <Route path="/app" element={<AppScreen/>}/>
+        <Route path='*' element={
+          <PrivateRouter log={log}>
+            <AppScreen/>
+          </PrivateRouter>
+        }/>
       </Routes>
-    </>
+    </Router>
   )
 }
 
