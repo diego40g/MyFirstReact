482fe27741b2f109729f99bb9439f47776a5c772
diego40g <diegopaznaula@hotmail.com>
2023-03-07 23:06:58 -0500

    only put the public routes login and register

--- 13-Firebase-Redux-Materialize/salary-calculation/src/routers/AuthRouter.jsx
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/routers/AuthRouter.jsx
@@ -1,35 +1,16 @@
-import React, {useEffect} from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-
-import {auth} from '../firebase/config-firebase'
+import { Route, Routes } from 'react-router-dom'
 
 import LoginScreen from '../pages/LoginScreen'
 import RegisterScreen from '../pages/RegisterScreen'
-import { login } from '../actions/auth'
-import AppRouter from './AppRouter'
+
 
 const AuthRouter = () => {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    auth.onAuthStateChanged(
-    (user) => {
-      if(user){
-        dispatch(login(user.uid, user.displayName))
-      }
-    })
-  }, [])
-  
+ 
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginScreen/>}/>
-          <Route path="/register" element={<RegisterScreen/>}/>
-        </Routes>
-        <AppRouter/>
-      </Router>
-    </>
+    <Routes>
+      <Route path="/login" element={<LoginScreen/>}/>
+      <Route path="/register" element={<RegisterScreen/>}/>
+    </Routes>  
   )
 }
 
