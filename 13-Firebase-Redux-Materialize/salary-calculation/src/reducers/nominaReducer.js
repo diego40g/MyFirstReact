72f45c847e4d7365b3ed9d4adaf2929c4fd860e4
diego40g <diegopaznaula@hotmail.com>
2023-03-08 23:30:42 -0500

    creating nominaReducer who save the salaru calculation

---
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/reducers/nominaReducer.js
@@ -0,0 +1,14 @@
+/**
+ * {
+ *  id: "asdnk123nk1kln1",
+ *  fecha: "08/03/2023"
+ *  pago: 3000.00
+ * }
+ *  */
+export const nominaReducer = (state = {}, action) => {
+    switch (action.type) {
+        
+        default:
+            return state;
+    }
+}
