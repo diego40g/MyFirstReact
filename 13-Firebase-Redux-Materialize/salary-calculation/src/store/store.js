2244794e51814bc4c71ca2ad793db277bd995f5d
diego40g <diegopaznaula@hotmail.com>
2023-03-08 23:31:10 -0500

    adding into reducers nomina for using

--- 13-Firebase-Redux-Materialize/salary-calculation/src/store/store.js
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/store/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
 import { authReducer } from '../reducers/authReducer'
+import { nominaReducer } from '../reducers/nominaReducer';
 import thunk from 'redux-thunk';
 
 const reducers = combineReducers({
     auth: authReducer,
+    nomina: nominaReducer,
 })
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
