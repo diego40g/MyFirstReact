35b598f39d3514fbdc2f52b49eb5063468108643
diego40g <diegopaznaula@hotmail.com>
2023-03-09 23:28:13 -0500

    adding the component form add

--- 13-Firebase-Redux-Materialize/salary-calculation/src/pages/AppScreen.jsx
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/pages/AppScreen.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 import Navbar from '../components/Navbar'
+import FormAdd from '../components/FormAdd'
 
 const AppScreen = () => {
   const {auth} = useSelector( (state) => state )
+  
   return (
     <>
         <Navbar/>
         <div className="container">
           <h1 className='center'>Hola {auth.displayname}</h1>
           <hr />
+          <FormAdd />
         </div>
     </>
   )
