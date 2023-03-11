a74523f28e28f40b4ab64c72689a9d30d62ed87e
diego40g <diegopaznaula@hotmail.com>
2023-03-10 22:59:36 -0500

    push info of firestore to data array

--- 13-Firebase-Redux-Materialize/salary-calculation/src/helpers/loadData.js
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/helpers/loadData.js
@@ -2,11 +2,14 @@ import { getDocs, db, collection } from '../firebase/config-firebase'
 export const loadData = async(uid) => {
     const path = collection(db, `${uid}`, "nominas", "nomina")
     const nominaList = await getDocs(path)
-    nominaList.forEach((result) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(result.id, " => ", result.data());
-    });
-    //const data= nominaList.data()
     const data= []
+    nominaList.forEach((nomina) => {
+        const nominaData = nomina.data()
+        data.push({
+            id: nomina.id,
+            ...nominaData
+        })
+    });
+    
     return data
 }
