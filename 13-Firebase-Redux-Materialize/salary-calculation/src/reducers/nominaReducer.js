c8aca0e8ea0654104856ec170d8fb55969620348
diego40g <diegopaznaula@hotmail.com>
2023-03-10 23:12:27 -0500

    adding case nomina read  of pass the state when update and the data in payload

--- 13-Firebase-Redux-Materialize/salary-calculation/src/reducers/nominaReducer.js
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/reducers/nominaReducer.js
@@ -10,6 +10,11 @@ export const nominaReducer = (state = {}, action) => {
     switch (action.type) {
         case types.nominaAdd:
             return {};
+        case types.nominaRead:
+            return {
+                ...state,
+                data: action.payload
+            };
         default:
             return state;
     }
