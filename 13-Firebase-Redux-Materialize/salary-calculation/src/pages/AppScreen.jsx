9ab2727475d2de46b9e6aa1b51c502500a8948f6
diego40g <diegopaznaula@hotmail.com>
2023-03-11 23:40:06 -0500

    adding a basic struct of table

--- 13-Firebase-Redux-Materialize/salary-calculation/src/pages/AppScreen.jsx
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/pages/AppScreen.jsx
@@ -3,9 +3,11 @@ import { useSelector } from 'react-redux'
 
 import Navbar from '../components/Navbar'
 import FormAdd from '../components/FormAdd'
+import Element from '../components/Element'
 
 const AppScreen = () => {
   const {auth} = useSelector( (state) => state )
+  const data = useSelector((state) => state.nomina.data)
   
   return (
     <>
@@ -14,6 +16,22 @@ const AppScreen = () => {
           <h1 className='center'>Hola {auth.displayname}</h1>
           <hr />
           <FormAdd />
+          <table>
+            <thead>
+              <tr>
+                <th>Fecha</th>
+                <th>Cantidad</th>
+                <th>Borrar</th>
+              </tr>
+            </thead>
+            <tbody>
+            {
+              data.map((element) => {
+                  return <Element key={element.id} data={element}/>
+              })
+            }
+            </tbody>
+          </table>
         </div>
     </>
   )
