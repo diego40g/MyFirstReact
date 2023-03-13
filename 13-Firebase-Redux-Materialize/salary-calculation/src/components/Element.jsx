2dd1a8c07f6ff4a67b00d9ef0c38d9d3fe4622d1
diego40g <diegopaznaula@hotmail.com>
2023-03-12 23:16:31 -0500

    validation using fecha for diferent tipys fibase or localdatastring

--- 13-Firebase-Redux-Materialize/salary-calculation/src/components/Element.jsx
+++ 13-Firebase-Redux-Materialize/salary-calculation/src/components/Element.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
-const Element = ({data}) => {
-  const { fecha, pago} = data
+const Element = ({ data }) => {
+  const { fecha, pago } = data
+  let fechaFormato
   //const fechaFormato = `${fecha.toDate().getFullYear()}-${fecha.toDate().getMonth()}-${fecha.toDate().getDate()}`
-  const fechaFormato = fecha.toDate().toLocaleDateString("es-EC")
+  if (fecha.seconds) {
+    const date = fecha.toDate()
+    fechaFormato = date.toLocaleDateString()
+  }else{
+    fechaFormato= fecha
+  }
   return (
     <>
         <td>{fechaFormato}</td>
-        <td>{pago}</td>
+        <td>${pago}</td>
         <td>
             <button className="btn red">Borrar</button>
         </td>
