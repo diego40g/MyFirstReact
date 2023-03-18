651ae24c58faee506071bb8d51c67bdf47852398
diego40g <diegopaznaula@hotmail.com>
2023-03-17 23:27:42 -0500

    write two or more test into a describe

--- 14-TestingLibrary/testing-project/src/components/Counter.test.js
+++ 14-TestingLibrary/testing-project/src/components/Counter.test.js
@@ -1,11 +1,18 @@
 import { render } from "@testing-library/react"
 import Counter from "./Counter"
 
-test("Pruebas al componente Counter", () => {
-    const wrapper = render(<Counter />);
-
-    // console.log(wrapper.getByText("Counter: 0").tagName);
-
-    //ACTUAL
-    expect(wrapper.getByText("Counter: 0").tagName).toBe("H2")
+describe("Probar el componente Counter", () => {
+    it("Pruebas de que el titulo y el estado del componente Counter se renderizan correctamente", () => {
+        const wrapper = render(<Counter />);
+    
+        // console.log(wrapper.getByText("Counter: 0").tagName);
+    
+        //ACTUAL
+        expect(wrapper.getByText("Counter: 0").tagName).toBe("H2")
+    })
+    
+    it("Prueba que 1 + 1 es 2 y no es 3", () => {
+        expect(1 + 1).toBe(2)
+        expect(1 + 1).not.toBe(3)
+    })
 })
