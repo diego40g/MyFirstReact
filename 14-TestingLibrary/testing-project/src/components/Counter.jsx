52adaf65703aa7950f387d8064bb0ed2cda503cd
diego40g <diegopaznaula@hotmail.com>
2023-03-17 23:37:34 -0500

    adding fucntion handle Add to add one at counter

--- 14-TestingLibrary/testing-project/src/components/Counter.jsx
+++ 14-TestingLibrary/testing-project/src/components/Counter.jsx
@@ -2,9 +2,16 @@ import React, { useState } from 'react'
 
 const Counter = () => {
   const [counter, setCounter] = useState(0)
+
+  const handleAdd = () => {
+    setCounter(counter+1)
+  }
   return (
     <>
         <h2>Counter: {counter}</h2>
+
+        <button onClick={handleAdd}>+1</button>
+        <button>-1</button>
     </>
   )
 }
