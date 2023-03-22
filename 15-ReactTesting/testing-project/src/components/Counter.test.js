39b17899ec031f7855a403fe9df53002df91e903
diego40g <diegopaznaula@hotmail.com>
2023-03-21 23:20:01 -0500

    solve some erros

--- 15-ReactTesting/testing-project/src/components/Counter.test.js
+++ 15-ReactTesting/testing-project/src/components/Counter.test.js
@@ -3,19 +3,23 @@ import Counter from "./Counter"
 import userEvent from "@testing-library/user-event";
 
 describe("<Counter />", () => {
-    render(<Counter />);
     
-    //beforEach(() => {})
+    // render(<Counter />);
 
-    afterEach(() => {
-        userEvent.click(screen.getByLabelText("reset"))
-    })
+    //beforeEach(() => {})
+
+    /*beforeEach(async() => {
+        const reset = screen.getByText("Reset")
+        await fireEvent.click(reset)
+    })*/
 
     it("Primer snapshot", () => {
+        render(<Counter />);
         expect(screen.getByRole("counterRole")).toMatchSnapshot()
     })
 
     it("Pruebas de que el titulo y el estado del componente Counter se renderizan correctamente", () => {
+        render(<Counter />);
         // console.log(wrapper.getByText("Counter: 0").tagName);
     
         //ACTUAL
@@ -23,7 +27,7 @@ describe("<Counter />", () => {
     })
     
     it("Verificar que el click +1 aumente correctamente", () => {
-        render(<Counter/>)
+        render(<Counter />);
         //const btn = screen.getByText("+1")
         const btn = screen.getByLabelText("aumentar")
 
@@ -32,7 +36,7 @@ describe("<Counter />", () => {
     })
 
     it("Verificar que el click -1 aumente correctamente", () => {
-        render(<Counter/>)
+        render(<Counter />);
         const btn = screen.getByLabelText("disminuir")
 
         fireEvent.click(btn)
@@ -40,14 +44,14 @@ describe("<Counter />", () => {
     })
 
     it("Verificar que el click vuelve al valor inicial", () => {
-        render(<Counter/>)
+        render(<Counter />);
 
         userEvent.click(screen.getByLabelText("reset"))
         expect(screen.getByRole("counterRole").textContent).toContain("Counter: 0")
     })
 
     it("Simular que alguin esta usando la app", () => {
-        render(<Counter/>)
+        render(<Counter />);
         const btnAdd = screen.getByLabelText("aumentar")
         const btnDis = screen.getByLabelText("disminuir")
         
