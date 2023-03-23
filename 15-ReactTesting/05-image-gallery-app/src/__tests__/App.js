fcf2543b736d60cb9ea2e1aa56c12c4d64a60d2e
diego40g <diegopaznaula@hotmail.com>
2023-03-22 23:45:25 -0500

    create the first test with snapshot to app component

---
+++ 15-ReactTesting/05-image-gallery-app/src/__tests__/App.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react"
+import App from '../App'
+
+test('Snapshot del app', () => { 
+    render(<App/>)
+    
+    expect(screen.getByRole('app')).toMatchSnapshot()
+})
