6f8bd376d8684a29f06776965889550e5a66847b
diego40g <diegopaznaula@hotmail.com>
2023-03-24 23:49:16 -0500

    test for cards who was diferents elements card

---
+++ 15-ReactTesting/05-image-gallery-app/src/__tests__/Cards.js
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import Cards from "../components/Cards"
+
+describe('<Container />', () => { 
+    it("Snapshot principal", () => {
+        render(<Cards />)
+
+        expect(screen.getByLabelText("imagenes")).toMatchSnapshot()
+    })
+
+    it("Snapshot card imagen", async() => {
+        render(<Cards />)
+
+        const response =await screen.findAllByLabelText("img")
+
+        //expect(response.length).toBe(10)
+        expect(response).toHaveLength(10)
+    })
+})
